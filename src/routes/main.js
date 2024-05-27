c868a5f4c95abbd55ec827c8367733698aa00504
Pratham2110 <prathamhole@gmail.com>
2024-05-27 16:17:59 +0530

    NEW COMMIT

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,10 +3,14 @@ const { route } = require('express/lib/application')
 const routes=express.Router()
 const Detail=require("../models/details")
 
-routes.get('/',(request,response)=>{
+routes.get('/', (request,response)=>{
     response.render("index")
     const details = Detail.findOne({"_id":"6653ba770b12a87a0bbe77fd"})
     console.log(details)
+
+    // response.render("index",{
+    //     details:details
+    // })
 })  
 
 
